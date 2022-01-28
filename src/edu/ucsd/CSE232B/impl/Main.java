3fad0c271f66b2dc15ff5a2fa5072d0a5b7e5f5b
AntiZpvoh <wwwqyy@126.com>
2022-01-28 00:35:39 -0800

    Add test code and implement rule (1)(2)(3)

--- src/edu/ucsd/CSE232B/impl/Main.java
+++ src/edu/ucsd/CSE232B/impl/Main.java
@@ -1,7 +1,27 @@
 package edu.ucsd.CSE232B.impl;
 
+import edu.ucsd.CSE232B.parsers.ExpressionGrammarLexer;
+import edu.ucsd.CSE232B.parsers.ExpressionGrammarParser;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.w3c.dom.Node;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        final String expression = "doc(\"j_caesar.xml\")//time";
 
+        final ExpressionGrammarLexer lexer = new ExpressionGrammarLexer(CharStreams.fromString(expression));
+        final CommonTokenStream tokens = new CommonTokenStream(lexer);
+        final ExpressionGrammarParser parser = new ExpressionGrammarParser(tokens);
+
+        final ParseTree tree = parser.ap();
+        final XPathParser xPathParser = new XPathParser();
+        final ArrayList<Node> nodes = xPathParser.visit(tree);
+        for(Node node: nodes){
+            System.out.println(node.getTextContent());
+        }
     }
 }
--- src/edu/ucsd/CSE232B/impl/XPathParser.java
+++ src/edu/ucsd/CSE232B/impl/XPathParser.java
@@ -4,7 +4,9 @@ import edu.ucsd.CSE232B.parsers.ExpressionGrammarBaseVisitor;
 import edu.ucsd.CSE232B.parsers.ExpressionGrammarParser;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -13,6 +15,8 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class XPathParser extends ExpressionGrammarBaseVisitor<ArrayList<Node>> {
     private Document xmlDocument;
@@ -26,26 +30,55 @@ public class XPathParser extends ExpressionGrammarBaseVisitor<ArrayList<Node>> {
         this.xmlDocument = xmlDocument;
     }
 
+    @Override public ArrayList<Node> visitDirectAbsolutePath(ExpressionGrammarParser.DirectAbsolutePathContext ctx) {
+        visitDoc(ctx.doc());
+
+        return visit(ctx.rp());
+    }
+
+    @Override public ArrayList<Node> visitIndirectAbsolutePath(ExpressionGrammarParser.IndirectAbsolutePathContext ctx) {
+        visitDoc(ctx.doc());
+
+        ArrayList<Node> result = new ArrayList<>();
+        LinkedList<Node> bfsQueue = new LinkedList<>();
+        bfsQueue.addLast(xmlDocument.getDocumentElement());
+        while(!bfsQueue.isEmpty()){
+            this.currentNode = bfsQueue.getFirst();
+            result.addAll(visit(ctx.rp()));
+            for(int i=0; i<this.currentNode.getChildNodes().getLength(); i++){
+                bfsQueue.addLast(this.currentNode.getChildNodes().item(i));
+            }
+            bfsQueue.removeFirst();
+        }
+        return result;
+    }
+
     @Override public ArrayList<Node> visitDoc(ExpressionGrammarParser.DocContext ctx) {
         try {
-            File xmlFile = new File(ctx.fileName().getText());
+            File xmlFile = new File(ctx.fileName().getText().substring(1, ctx.fileName().getText().length()-1));
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
             this.xmlDocument = builder.parse(xmlFile);
             this.currentNode = xmlDocument.getDocumentElement();
-
-            if(ctx.getChild(1).getText().equals("/")){
-                ParseTree rp = ctx.getChild(2);
-                return visit(rp);
-            }else{
-
-            }
-
         } catch (ParserConfigurationException | SAXException | IOException e) {
             e.printStackTrace();
         }
 
-        return visit(ctx.getChild(1));
+        return null;
+    }
+
+    @Override public ArrayList<Node> visitTagRelativePath(ExpressionGrammarParser.TagRelativePathContext ctx) {
+        ArrayList<Node> result = new ArrayList<>();
+        String tagName = ctx.getText();
+
+        if(currentNode instanceof Element){
+            Element currentElement = (Element)this.currentNode;
+            if(currentElement.getTagName().equals(tagName)){
+                result.add(this.currentNode);
+            }
+        }
+
+        return result;
     }
 
 
